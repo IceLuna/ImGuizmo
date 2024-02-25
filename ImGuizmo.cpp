45d91dab94c9e700047a92a23625938b2c71ba4b
IceLuna <shikhali00@mail.ru>
2024-02-25 12:32:41 +0100

    Eagle-specific. Updated mask info

--- ImGuizmo.cpp
+++ ImGuizmo.cpp
@@ -769,8 +769,8 @@ namespace IMGUIZMO_NAMESPACE
    static const char* translationInfoMask[] = { "X : %5.3f", "Y : %5.3f", "Z : %5.3f",
       "Y : %5.3f Z : %5.3f", "X : %5.3f Z : %5.3f", "X : %5.3f Y : %5.3f",
       "X : %5.3f Y : %5.3f Z : %5.3f" };
-   static const char* scaleInfoMask[] = { "X : %5.2f", "Y : %5.2f", "Z : %5.2f", "XYZ : %5.2f" };
-   static const char* rotationInfoMask[] = { "X : %5.2f deg %5.2f rad", "Y : %5.2f deg %5.2f rad", "Z : %5.2f deg %5.2f rad", "Screen : %5.2f deg %5.2f rad" };
+   static const char* scaleInfoMask[] = { "X : %5.3f", "Y : %5.3f", "Z : %5.3f", "XYZ : %5.3f" };
+   static const char* rotationInfoMask[] = { "X : %5.3f deg %5.3f rad", "Y : %5.3f deg %5.3f rad", "Z : %5.3f deg %5.3f rad", "Screen : %5.3f deg %5.3f rad" };
    static const int translationInfoIndex[] = { 0,0,0, 1,0,0, 2,0,0, 1,2,0, 0,2,0, 0,1,0, 0,1,2 };
    static const float quadMin = 0.5f;
    static const float quadMax = 0.8f;
